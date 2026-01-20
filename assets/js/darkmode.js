ad5387f603af81ac36f2566c52c3fd8137e3a84e
Not-Krevin <kevinhiggins1333@gmail.com>
2026-01-19 23:03:13 -0800

    Create darkmode.js

---
+++ assets/js/darkmode.js
@@ -0,0 +1,15 @@
+const toggle = document.querySelector(".dark-mode-toggle");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+toggle.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
